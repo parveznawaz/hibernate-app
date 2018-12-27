8b1d5191fc43ed56c954975d96ed757bb13fc05d
parveznawaz <parvez.nawaz@gmail.com>
2018-12-26 19:34:57 -0500

    comment

---
+++ src/main/java/com/wise/jpa/hibernate/hibernateapp/HibernateAppApplication.java
@@ -0,0 +1,14 @@
+package com.wise.jpa.hibernate.hibernateapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateAppApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/wise/jpa/hibernate/hibernateapp/HibernateAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.wise.jpa.hibernate.hibernateapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HibernateAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
